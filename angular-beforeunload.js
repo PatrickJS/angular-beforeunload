01989a4efd960e932378b2d5f9523b1bb9b56d00
PatrickJS <github@gdi2290.com>
2013-10-17 10:50:42 -0700

    update source

--- angular-beforeunload.js
+++ angular-beforeunload.js
@@ -1,22 +1,28 @@
 'use strict';
 
-angular.module('angular-onbeforeunload', [])
-  .factory('onBeforeUnload', ['$window',
+angular.module('angular-beforeunload', [])
+  .factory('BeforeUnload', ['$window',
     function($window) {
       var leavingPageText = "You'll lose your changes if you leave";
       var leavingPageText2 = "Are you sure you want to leave this page?";
+      
       function init(top, bottom) {
         leavingPageText = top || leavingPageText;
         leavingPageText2 = bottom || leavingPageText2;
-        $window.onbeforeunload = function(){
+        
+        $window.onbeforeunload = function(e){
           return leavingPageText;
         }
+        
         return function(event, next, current) {
-          if(!confirm(leavingPageText + "\n\n"+leavingPageText2)) {
+          if (!confirm(leavingPageText + "\n\n"+leavingPageText2)) {
             event.preventDefault();
-          }
+          } else {
+            $window.onbeforeunload = null;
+          } 
         };
       }
+      
       return {
         init: init
       }
